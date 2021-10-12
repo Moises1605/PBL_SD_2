ca7ab682dd39aca1b32f4a2c69d326bbf3f71e79
Moises1605 <moisesalmeida123@hotmail.com>
2021-10-12 10:07:43 -0300

    Adicionando os publish e subscribes

--- casa.c
+++ casa.c
@@ -1,3 +1,18 @@
+/*
+5.1. O acionamento remoto deve ser feito por meio de uma página Web, que deve
+apresentar no mínimo:
+    5.1.1. Estado atual das lâmpadas da residência;
+    5.1.2. Acionamento remoto da iluminação dos ambientes internos. Esse
+    acionamento deve ser programável;
+    5.1.3. Ativação e desativação remota do alarme;
+    5.1.4. Estado atual do alarme e notificação de uma possível invasão;
+    5.1.5. Histórico mensal do alarme, contendo a data, o horário e o tipo de evento
+    ocorrido;
+    5.1.6. Estado atual do ar-condicionado e a temperatura atual da sala;
+    5.1.7. Interface para ajuste da faixa de operação do ar-condicionado.
+    5.2. Usar recursos do ambiente Amazon Web Services (AWS).
+*/
+
 #include <stdio.h>
 #include <stdlib.h> 
 #include <wiringPi.h>
@@ -43,14 +58,56 @@ const static char* topic[TOPIC_NUM] = {
     "sensorPJ ",
     "faixaOpI",
     "faixaOpS",
-    "iluminacao1",
-    "iluminacao2",
-    "iluminacao3",
+    "iluminacaoInterna",
+    "sensorPresenca",
+    "temperatura",
+    "iluminacaoGaragem",
+    "iluminacaoJardim"
+};
 
-}; 
+
+char saida_iluminacao_interna[];
+char saida_Saida_alarme[];
+char saida_ar_condicionado[];
+
+/*Lista de entradas do programa*/
+int sensor_PJ[];
+int sensor_presenca;
+float entrada_temperatura = 17;
+float faixa_operacao_inferior = 17;
+float faixa_operacao_superior = 23;
+int entrada_iluminacao_interna;
+int entrada_alarme;
+
+
+char saida_ar_condicionado[] = "";
+char saida_iluminacao_jardim[] = "";
+char saida_iluminacao_Garagem[] = "";
+
+
 
 int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
     char* payload = message->payload;
+
+    if(topicName == "arCondicionado"){
+        saida_ar_condicionado = message->payload;
+    }else if(topicName == "alarme"){
+        saida_Saida_alarme = message->payload;
+    }else if(topicName == "faixaOpI"){
+        faixa_operacao_inferior =  atof(message->payload);
+    }else if(topicName == "faixaOpS"){
+        faixa_operacao_superior = atof(message->payload);
+    }else if(topicName == "iluminacaoInterna"){
+        saida_iluminacao_interna = message->payload;
+    }else if(topicName == "sensorPJ"){
+        sensor_PJ = message->payload - '0';
+    }else if(topicName == "sensorPresenca"){
+        sensor_presenca = message->payload - '0';
+    }else if(topicName == "estadoIlumincaoInterna"){
+        entrada_iluminacao_interna = message->payload - '0';
+    }else if(topicName == "estadoAlarme"){
+        entrada_alarme = message->payload - '0';
+    }
  
     /* Mostra a mensagem recebida */
     printf("Mensagem recebida! \n\rTopico: %s Mensagem: %s\n", topicName, payload);
@@ -64,6 +121,13 @@ int on_message(void *context, char *topicName, int topicLen, MQTTClient_message 
 }
 
 int main(){ 
+    
+    MQTTBegin();
+
+    MQTTSubscribe(TOPIC_ENTRADA_ILUMINACAO_I);
+    MQTTSubscribe(TOPIC_ENTRADA_ALARME);
+    MQTTSubscribe(TOPIC_ENTRADA_FAIXA_S);
+    MQTTSubscribe(TOPIC_ENTRADA_FAIXA_I);
     
 
     int lcd;
@@ -87,83 +151,88 @@ int main(){
     int horario;
     int faixa_operacao_S;
     int faixa_operacao_I;
-    int temopar
-
-    /*Lista de entradas do programa*/
-    char saida_iluminacao_interna[];
-    char saida_Saida_alarme[];
-    char saida_ar_condicionado[];
-
-    char saida_sensot_PJ[] = "";
-    char saida_ar_condicionado[] = "";
-    char saida_iluminacao_jardim[] = "";
-    char saida_iluminacao_Garagem[] = "";
+    int temopar;
+    int timeout;
 
     //Para manter um loop infinito
     MQTTClient_setCallbacks(client, NULL, NULL, on_message, NULL);
     while(1){ 
+        entrada_temperatura = digitalRead(POTEN_SDA) * 10;
+        if(BUTTON_1 == HIGH){
+            entrada_temperatura = digitalRead(POTEN_SCL) * 10;
+        }
+        if(BUTTON_2){
+            entrada_temperatura = digitalRead(POTEN_SCL) * 10;
+        }
+        
+        //fazer a conversão de tipos para esses valores
+        MQTTPublish(TOPIC_TEMPERATURA, entrada_temperatura);
+        MQTTPublish(TOPIC_OPERACAO_INFERIOR, faixa_operacao_inferior);
+        MQTTPublish(TOPIC_OPERACAO_SUPERIOR, faixa_operacao_superior);
+
 
         //iluminacao_garagem
-        if(digitalRead(27) == HIGH && !(horario > 6 && horario < 18)){
-            saida_iluminacao_Garagem = 1;
-            MQTTPublish(TOPIC_LUZ_Garagem, "test maqiatto.com!");
+        if((digitalRead(DIP_2) == HIGH || sensor_presenca == 1) && !(horario > 6 && horario < 18)){
+            //saida_iluminacao_Garagem = 1;
+            MQTTPublish(TOPIC_LUZ_GARAGEM, "ligado");
         }else{
-            saida_iluminacao_Garagem = 0;
-            MQTTPublish(TOPIC_LUZ_Jardim, "test maqiatto.com!");
+            //saida_iluminacao_Garagem = 0;
+            MQTTPublish(TOPIC_LUZ_GARAGEM, "desligado");
         }
 
         //iluminacai_jardim
         if(horario >= 18 && horario <= 23){
-            saida_iluminacao_jardim = 1;
-            MQTTPublish(TOPIC, "test maqiatto.com!");
+            //saida_iluminacao_jardim = 1;
+            MQTTPublish(TOPIC_LUZ_JARDIM, "ligado");
         }else{
-            saida_iluminacao_jardim = 0;
-            MQTTPublish(TOPIC, "test maqiatto.com!");
+            //saida_iluminacao_jardim = 0;
+            MQTTPublish(TOPIC_LUZ_JARDIM, "desligado");
         }
 
 
         //iluminação interna
-        if(digitalRead(27) == HIGH){
-            saida_iluminacao_interna = 1;
+        if(digitalRead(DIP_2) == HIGH || entrada_iluminacao_interna == 1){
+            //saida_iluminacao_interna = 1;
             saida_iluminacao_interna = "Luz L"
-            MQTTPublish(TOPIC, "test maqiatto.com!");
+            MQTTPublish(TOPIC_LUZ_INTERNA, "ligado");
         }else{
-            saida_iluminacao_interna = 0;
+            //saida_iluminacao_interna = 0;
             saida_iluminacao_interna = "Luz D"
-            MQTTPublish(TOPIC, "test maqiatto.com!");
+            MQTTPublish(TOPIC_LUZ_INTERNA, "desligado");
         } 
 
         //alarme
-        if((digitalRead(22) == HIGH)  || (digitalRead(27) == HIGH) && (digitalRead(4) == HIGH)){
+        if((digitalRead(DIP_1) == HIGH) || (digitalRead(DIP_2) == HIGH || entrada_alarme == 1 || 
+            sensor_PJ == 1) && (digitalRead(DIP_3) == HIGH)){
             saida_Saida_alarme = "Alarme L";
-            MQTTPublish(TOPIC, "test maqiatto.com!");
+            MQTTPublish(TOPIC_ALARME, "ligado");
         }else{
             saida_Saida_alarme = "Alarme D";
-            MQTTPublish(TOPIC, "test maqiatto.com!");
+            MQTTPublish(TOPIC_ALARME, "desligado");
         } 
 
         //ar condicionado
         if(temopar){
-            if(faixa_operacao_I >= faixa_operacao_S){
+            if(faixa_operacao_inferior >= faixa_operacao_superior){
                 saida_ar_condicionado = "ar D";
-                MQTTPublish(TOPIC, "test maqiatto.com!");
+                MQTTPublish(TOPIC_ARCONDICIONADO, "desligado");
             }else{
-                if(digitalRead(27) == HIGH){
-                    if(entrada_temperatura <= faixa_operacao_I){
+                if(digitalRead(DIP_2) == HIGH){
+                    if(entrada_temperatura <= faixa_operacao_inferior){
                         saida_ar_condicionado = "ar D";
-                        MQTTPublish(TOPIC, "test maqiatto.com!");
-                    }else if(entrada_temperatura >= faixa_operacao_S){
+                        MQTTPublish(TOPIC_ARCONDICIONADO, "desligado");
+                    }else if(entrada_temperatura >= faixa_operacao_superior){
                         saida_ar_condicionado = "ar L";
-                        MQTTPublish(TOPIC, "test maqiatto.com!");
+                        MQTTPublish(TOPIC_ARCONDICIONADO, "ligado");
                     }
                 }else{
                     saida_ar_condicionado = "ar D";
-                    MQTTPublish(TOPIC, "test maqiatto.com!");
+                    MQTTPublish(TOPIC_ARCONDICIONADO, "desligado");
                 }
             }
             if(entrada_temperatura == 17){
                 saida_ar_condicionado = "ar D";
-                MQTTPublish(TOPIC, "test maqiatto.com!");
+                MQTTPublish(TOPIC_ARCONDICIONADO, "desligado");
                 temopar = 0;
                 
             }
@@ -186,10 +255,12 @@ int main(){
         lcdPuts(lcd,saida_iluminacao_interna);
         lcdPosition(lcd, 0, 1);
         lcdPuts(lcd,saida_Saida_alarme);
+        sleep(5000 / 1000);
         /*Por padrão, o texto é impresso na tela na linha superior, segunda coluna. Para alterar a posição, use 
         lcdPosition (lcd, COLUMN, ROW).Em um LCD 16 × 2, as linhas são numeradas de 0 a 1 e as colunas são numeradas de 0 a 15.*/
     }
 
+    MQTTDisconnect();
     return 0;
 } 
 
